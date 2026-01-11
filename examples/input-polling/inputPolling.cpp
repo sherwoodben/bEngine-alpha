a039edb36b4d4900fc519b10359843c5c9424a4c
Benjamin Sherwood <28694955+sherwoodben@users.noreply.github.com>
2026-01-11 01:56:22 -0600

    wip: tweaked example/added comments

--- examples/input-polling/inputPolling.cpp
+++ examples/input-polling/inputPolling.cpp
@@ -10,25 +10,29 @@
 #include <format>
 #include <vector>
 
+/// @brief we'll use the bEngine namespace just so we don't need to prepend everything with bEngine::
+/// @note this probably isn't the 'best' practice!
+using namespace bEngine;
+
 namespace
 {
     /// @brief store the handle for the main window
-    unsigned int mainWindowHandle{0};
+    unsigned int g_mainWindowHandle{0};
 } // namespace
 
-/// @brief initializes the "user data" associated with the application
+/// @brief initializes the main window and stores the (global) handle to the main window
 ///
 /// in other words, this is a place to configure data/load resources you will use in the remaining user defined
 /// functions!
 /// @param app a reference to the application which is owned by the library; useful for setting up window(s), etc.
 /// @return true if initalization/loading was successful and the program can continue, or false if the program
 /// should close/crash/terminate
-const bool initialize(bEngine::bEngineApp *const app)
+const bool initialize(bEngineApp *const app)
 {
     // create a window for this example, when it is closed the application will terminate... but we can't rely on the
     // engine to do this automatically in this example!
     INFO_MSG("Creating a window for the example project.");
-    mainWindowHandle = app->add_window(bEngine::bEngineWindow::create_window(1280, 720));
+    g_mainWindowHandle = app->add_window(bEngineWindow::create_window(1280, 720));
 
     // assume all other initialization went well; this is just the barebones example after all; return true so the
     // app continues to load/initialize
@@ -43,94 +47,101 @@ const bool initialize(bEngine::bEngineApp *const app)
 void update(const double deltaTime)
 {
     // we have to be sure to quit the app if the window is closed...
-    const auto mainWindow = bEngine::get_app().get_window(mainWindowHandle);
+    const auto mainWindow = get_app().get_window(g_mainWindowHandle);
     if (!mainWindow)
     {
         INFO_MSG("The main window has been closed. Quitting the application.");
-        bEngine::get_app().quit();
+        get_app().quit();
         return;
     }
 
-    const auto inputState = mainWindow->get_input_state();
+    // now we want to get the input state associated with the main window:
+    const auto &inputState = mainWindow->get_input_state();
 
-    std::vector<std::string>  pressedKeyNames;
-    std::vector<std::string>  pressedMouseButtonNames;
-    std::pair<double, double> mouseAxisValues;
+    // here's how we check if a specific key is pressed; in this case 'escape' which will quit the app.
+    if (inputState.get_key_state(bEngineKeyCode::key_escape))
+    {
+        INFO_MSG("Pressed 'escape'. Quitting the application.");
+        get_app().quit();
+        return;
+    }
 
-    for (bEngine::bEngineKeyCode keyCode = bEngine::bEngineKeyCode::first; keyCode != bEngine::bEngineKeyCode::last;
-         ++keyCode)
+    // keep a list of all of the names of the pressed keys/mouse buttons as well as the current mouse position
+    std::vector<std::string>  pressedKeyNames{};
+    std::vector<std::string>  pressedMouseButtonNames{};
+    std::pair<double, double> mouseAxisValues{0.0, 0.0};
+
+    // loop through all of the key codes bEngine recognizes and add the ones which are pressed to the list of pressed
+    // key names
+    for (bEngineKeyCode keyCode = bEngineKeyCode::first; keyCode != bEngineKeyCode::last; ++keyCode)
     {
         if (inputState.get_key_state(keyCode))
         {
-            pressedKeyNames.push_back(bEngine::get_keycode_name(keyCode));
+            pressedKeyNames.push_back(Input::get_keycode_name(keyCode));
         }
     }
 
-    for (bEngine::bEngineMouseButtonCode mouseButtonCode = bEngine::bEngineMouseButtonCode::first;
-         mouseButtonCode != bEngine::bEngineMouseButtonCode::last;
+    // do the same for the mouse buttons:
+    for (bEngineMouseButtonCode mouseButtonCode = bEngineMouseButtonCode::first;
+         mouseButtonCode != bEngineMouseButtonCode::last;
          ++mouseButtonCode)
     {
         if (inputState.get_mouse_button_state(mouseButtonCode))
         {
-            pressedMouseButtonNames.push_back(bEngine::get_mouse_button_code_name(mouseButtonCode));
+            pressedMouseButtonNames.push_back(Input::get_mouse_button_code_name(mouseButtonCode));
         }
     }
 
-    mouseAxisValues.first  = inputState.get_mouse_axis_state(bEngine::bEngineMouseAxisCode::mouse_x);
-    mouseAxisValues.second = inputState.get_mouse_axis_state(bEngine::bEngineMouseAxisCode::mouse_y);
+    // store the mouse position...
+    mouseAxisValues.first  = inputState.get_mouse_axis_state(bEngineMouseAxisCode::mouse_x);
+    mouseAxisValues.second = inputState.get_mouse_axis_state(bEngineMouseAxisCode::mouse_y);
 
-    static std::string               lastPressedKeysString{""};
-    static std::string               lastPressedMouseButtonsString{""};
-    static std::pair<double, double> lastMouseAxisValues;
-
+    // build strings which concatenate the pressed keys/mouse buttons into one string for convenience of printing to the
+    // console...
+    // we also want to keep track of the previous values so we don't spam the same values to the console when we're
+    // printing information!
     std::string pressedKeysString{""};
     std::string pressedMouseButtonsString{""};
 
+    static std::string               lastPressedKeysString{""};
+    static std::string               lastPressedMouseButtonsString{""};
+    static std::pair<double, double> lastMouseAxisValues{0.0, 0.0};
+
+    // build the pressed keys string
     for (const auto &name : pressedKeyNames)
     {
         pressedKeysString.append(name);
         pressedKeysString.append(1, ' ');
     }
 
+    // build the pressed mouse buttons string
     for (const auto &name : pressedMouseButtonNames)
     {
         pressedMouseButtonsString.append(name);
         pressedMouseButtonsString.append(1, ' ');
     }
 
+    // only update the last pressed keys string if the new value is different; likewise only print the new value if the
+    // value is different!
     if (pressedKeysString != lastPressedKeysString)
     {
         INFO_MSG(std::format("PRESSED KEYS: {}", pressedKeysString));
         lastPressedKeysString = pressedKeysString;
     }
 
+    // do the same for the mouse buttons
     if (pressedMouseButtonsString != lastPressedMouseButtonsString)
     {
         INFO_MSG(std::format("PRESSED MOUSE BUTTONS: {}", pressedMouseButtonsString));
         lastPressedMouseButtonsString = pressedMouseButtonsString;
     }
 
+    // and the mouse position
     if (mouseAxisValues != lastMouseAxisValues)
     {
         INFO_MSG(std::format("MOUSE POSITION: ({}, {})", mouseAxisValues.first, mouseAxisValues.second));
         lastMouseAxisValues = mouseAxisValues;
     }
-}
-
-/// @brief the tick function for the application which occurs as frequently as the inverse of the tick length
-/// @param tickLength the amount of time (in seconds) corresponding to one tick
-void tick(const double tickLength)
-{
-    // do nothing in this example
-}
-
-/// @brief frees the "user data" associated with the application
-/// @param app a const pointer to the const application which is owned by the library; const because there should be
-/// no need to actually impact the application at this point, just data which may have been created/allocated during
-/// the run of the program due to user actions
-void shutdown(const bEngine::bEngineApp *const app)
-{
-    // do nothing in this example
 }
 
 namespace bEngine
@@ -140,7 +151,7 @@ namespace bEngine
     /// @note since we provide an update function, the app will not automatically quit when all windows are closed. That
     /// means we need to account for when the main window closes ourself in the update function! The same would be true
     /// in the tick method if that were to be implemented instead of the update method
-    bEngineApp app{bEngineApp::create_app("Input Polling", initialize, update, 1.0 / 60.0, nullptr, shutdown)};
+    bEngineApp app{bEngineApp::create_app("Input Polling", initialize, update)};
 
     /// @brief returns an instance of the application class so the library can access the user-defined/configured
     /// application
